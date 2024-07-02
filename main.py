45a3c3ad25d026406fa00a63fada1fbe80fb2bff
Sayhan Rahman <sayhanrahman@gokkusagi.k12.tr>
2024-07-02 20:35:21 +0600

    Port binding issue fixed

--- main.py
+++ main.py
@@ -3,21 +3,17 @@ from pydantic import BaseModel
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import datetime
-
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
-
-id = os.environ['spoti_id']
-token = os.environ['spoti_token']
-
-# Spotify credentials
-SPOTIPY_CLIENT_ID = id
-SPOTIPY_CLIENT_SECRET = token
-
 
 # Initialize FastAPI app
 app = FastAPI()
+
+# Spotify credentials
+SPOTIPY_CLIENT_ID = os.getenv('spoti_id')
+SPOTIPY_CLIENT_SECRET = os.getenv('spoti_token')
 
 # Authenticate with Spotify
 auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
@@ -61,7 +57,6 @@ def search_artist(search_input: SearchInput):
     else:
         raise HTTPException(status_code=404, detail=f"No artists found for '{search_input.query}'.")
 
-
 @app.post("/search_track", response_model=list[Track])
 def search_track(search_input: SearchInput):
     # Search for similar tracks
@@ -81,16 +76,16 @@ def top_tracks_of_month():
     year = today.year
     month = today.month
 
-    # Get first and last day of the current month
-    first_day = datetime.date(year, month, 1)
-    last_day = datetime.date(year, month + 1, 1) - datetime.timedelta(days=1)
-
-    # Format dates for Spotify API
-    start_date = first_day.strftime('%Y-%m-%d')
-    end_date = last_day.strftime('%Y-%m-%d')
-
     # Fetch top tracks of the month
     results = sp.search(q=f'year:{year} month:{month}', type='track', limit=50)
     top_tracks = results['tracks']['items']
     return [{'name': track['name'], 'artists': ', '.join([artist['name'] for artist in track['artists']]), 'url': track['external_urls']['spotify']} for track in top_tracks]
 
+if __name__ == "__main__":
+    import uvicorn
+
+    # Get port number from environment variable PORT, defaulting to 8000 if not set
+    port = int(os.getenv("PORT", 8000))
+    
+    # Run the FastAPI application using Uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
